34f7403a3bd0e111d941e85808097f76acb8e89f
William <williamvdm@live.nl>
2026-04-07 07:26:37 +0200

    Add Dutch translations and improve text in nl.js; update French narrative in main.js

--- src/i18n/nl.js
+++ src/i18n/nl.js
@@ -35,12 +35,15 @@ export const nlText = {
     { key: 'courtoise', label: 'hoffelijk' },
     { key: 'courtois', label: 'hoffelijk' },
     { key: 'jaloux', label: 'jaloerse' },
+     { key: 'seigneur', label: 'heer' },
     { key: 'amour', label: 'liefde' },
     { key: 'dieu', label: 'god' },
     { key: 'piège', label: 'val' },
     { key: 'Muldumarec', label: 'Muldumarec' },
+    { key: 'merveilleuses', label: 'wonderlijke' },
+    { key: 'chevalier', label: 'ridder' },
   ],
-  bookText: `Sinds ik begonnen ben met het schrijven van lais, wil ik daar niet meer mee stoppen: ik zal alle avonturen die ik ken in verzen gieten. Daarom vertel ik eerst over Yonec, over de plaats waar hij werd geboren en over de ontmoeting van zijn ouders. De ridder die Yonec verwekte heette Muldumarec.
+  bookText: `Sinds ik begonnen ben met het schrijven van lais, wil ik daar niet meer mee stoppen: ik zal alle avonturen die ik ken in verzen gieten. Daarom vertel ik eerst over Yonec, over de plaats waar hij werd geboren en over de ontmoeting van zijn ouders. Degene die Yonec verwekte heette Muldumarec.
 
 Vroeger leefde er in Bretagne een zeer machtige oude heer. Hij was heer van Caerwent en erkend meester van het hele land. De stad lag aan de Duelas en ooit voeren er schepen langs. De heer was al erg oud. Omdat hij een grote erfenis zou achterlaten, nam hij een vrouw om kinderen te krijgen die hem konden opvolgen.
 
@@ -58,7 +61,7 @@ Ze had vaak gehoord dat er vroeger in dit land wonderlijke avonturen gebeurden d
 
 Nauwelijks had ze zo gesproken, of ze zag de schaduw van een grote vogel aan een smal venster. De vogel vloog de kamer in. Hij leek op een havik en had riempjes aan zijn poten. Hij ging voor haar zitten en veranderde na een tijdje in een mooie, gracieuze ridder. Ze schrok en bedekte haar hoofd met haar sluier.
 
-Maar de ridder sprak haar vriendelijk toe: "Mevrouw, wees niet bang. Ik ben gekomen om uw vriend te worden. Ik hou al lang van u. Nooit heb ik een andere vrouw bemind. Maar ik kon uw land niet verlaten en u niet bereiken zolang u mij niet riep. Nu kan ik bij u zijn."
+Maar de ridder sprak haar vriendelijk toe: "Wees niet bang, dame, de havik is een edele vogel!" Ik ben gekomen om uw vriend te worden. Ik hou al lang van u. Nooit heb ik een andere vrouw bemind. Maar ik kon uw land niet verlaten en u niet bereiken zolang u mij niet riep. Nu kan ik bij u zijn."
 
 De dame antwoordde dat ze hem graag als minnaar wilde aannemen, als hij in God geloofde en als hun liefde mogelijk was zonder zonde. Hij beleed toen zijn geloof en zei dat hij zelfs de hostie wilde ontvangen om haar gerust te stellen. Zij stemde toe.
 
@@ -127,15 +130,15 @@ Zo werd dit verhaal later een lai, om de pijn en het verdriet van deze twee geli
     },
     autour: {
       title: 'De havik',
-      image: 'https://fauconnerie-francaise.com/wp-content/uploads/2023/05/autour-des-palombes-1.jpg',
-      info: 'De havik is een roofvogel, net als de arend of de valk. Het is een snelle en wendbare jager die met hoge snelheid tussen de bomen door kan vliegen om zijn prooi te vangen.In de lai van Yonec verandert de ridder Muldumarec in een havik om de dame die opgesloten zit in haar toren te bezoeken. De havik symboliseert hier vrijheid, kracht en de onmogelijke liefde.',
-      quote: '"Nauwelijks had ze haar klacht uitgesproken, of ze zag de schaduw van een grote vogel in het smalle venster."',
+      image: 'https://www.moyenagepassion.com/wp-content/uploads/2020/12/epervier-fauconnerie-medievale-enluminure-672x372.jpg',
+      info: 'De havik is een roofvogel, net als de arend of de valk. Het is een snelle en wendbare jager die met hoge snelheid tussen de bomen door kan vliegen om zijn prooi te vangen.In de lai van Yonec verandert de ridder Muldumarec in een havik om de dame die opgesloten zit in haar toren te bezoeken. De havik symboliseert hier vrijheid, kracht en de onmogelijke liefde en wordt in het verhaal beschreven als een edele vogel.',
+      quote: '"Nauwelijks had ze haar klacht uitgesproken, of ze zag de schaduw van een grote vogel in het smalle venster." "Wees niet bang, dame, de havik is een edele vogel!"',
       source: "Voor de bron, klik op de Franse versie van de tekst",
     },
     bretagne: {
       title: 'Bretagne',
       image: '/images/afbbretagne.png',
-      info: 'Bretagne verwijst hier naar de regio\'s Ierland, Wales en Cornwall. In het lai van Yonec is het Bretonse wonderbaarlijke veel aanwezig, dat wil zeggen: Wonderbaarlijk register gebaseerd op reuzen, dwergen, draken, magie, betoverde bronnen, spookhuizen, magische ridders, feeën, wezens die zich kunnen metamorfoseren, zoals Muldumarec die van een vogel in een ridder kan veranderen.',
+      info: 'Bretagne verwijst hier naar de regio\'s Ierland, Wales en Cornwall. In het lai van Yonec is het Bretonse wonderbaarlijke veel aanwezig voor meer informatie zie: het wonderlijke.',
       quote: '"Vroeger leefde er in Bretagne een zeer machtige oude heer. Hij was heer van Caerwent en erkend meester van het hele land."',
       source: "Voor de bron, klik op de Franse versie van de tekst",
     },
@@ -150,36 +153,43 @@ Zo werd dit verhaal later een lai, om de pijn en het verdriet van deze twee geli
     courtoise: {
       title: 'Hoffelijkheid',
       image: 'https://www.moyenagepassion.com/wp-content/uploads/2024/09/amour-courtois-medieval-enluminure-codex-manesse.jpg',
-      info: 'Hoofs/Hoofse: ✣ kwaliteit verbonden aan adel // boerenwereld. Hoofsheld: ✣ Geheel van aristocratische (adellijke) waarden. De lais van Marie de France getuigen van het succes van deze verhalen in de aristocratische kringen van de twaalfde eeuw. In deze romantische literatuur, geschreven voor de hoven van edellieden, ontwikkelt zich een nieuw ideaal, het hoofse ideaal, dat nieuwe waarden verheerlijkt: verfijnde beleefdheid, matiging, vrijgevigheid en de verheerlijking van de vrouw.',
+      info: '✣ Hoofs/Hoofse: is een kwaliteit verbonden aan adel in tegenstelling tot de boerenwereld. ✣ Hoofsheld: Is het geheel van aristocratische (adellijke) waarden. De lais van Marie de France getuigen van het succes van deze verhalen in de aristocratische kringen van de twaalfde eeuw. In deze romantische literatuur, geschreven voor de hoven van edellieden, ontwikkelt zich een nieuw ideaal, het hoofse ideaal, dat nieuwe waarden verheerlijkt: verfijnde beleefdheid, matiging, vrijgevigheid en de verheerlijking van de vrouw.',
       quote: '"Het jonge meisje dat men hem gaf, was van hoge afkomst, wijs en hoffelijk, en uitzonderlijk mooi. Hij werd meteen op haar schoonheid verliefd."',
       source: "Voor de bron, klik op de Franse versie van de tekst",
     },
      courtois: {
       title: 'Hoffelijkheid',
       image: 'https://www.moyenagepassion.com/wp-content/uploads/2024/09/amour-courtois-medieval-enluminure-codex-manesse.jpg',
-      info: 'Hoofs/Hoofse: ✣ kwaliteit verbonden aan adel // boerenwereld. Hoofsheld: ✣ Geheel van aristocratische (adellijke) waarden. De lais van Marie de France getuigen van het succes van deze verhalen in de aristocratische kringen van de twaalfde eeuw. In deze romantische literatuur, geschreven voor de hoven van edellieden, ontwikkelt zich een nieuw ideaal, het hoofse ideaal, dat nieuwe waarden verheerlijkt: verfijnde beleefdheid, matiging, vrijgevigheid en de verheerlijking van de vrouw.',
+      info: '✣ Hoofs/Hoofse: is een kwaliteit verbonden aan adel in tegenstelling tot de boerenwereld. ✣ Hoofsheld: Is het geheel van aristocratische (adellijke) waarden. De lais van Marie de France getuigen van het succes van deze verhalen in de aristocratische kringen van de twaalfde eeuw. In deze romantische literatuur, geschreven voor de hoven van edellieden, ontwikkelt zich een nieuw ideaal, het hoofse ideaal, dat nieuwe waarden verheerlijkt: verfijnde beleefdheid, matiging, vrijgevigheid en de verheerlijking van de vrouw.',
       quote: '"Het jonge meisje dat men hem gaf, was van hoge afkomst, wijs en hoffelijk, en uitzonderlijk mooi. Hij werd meteen op haar schoonheid verliefd."',
       source: "Voor de bron, klik op de Franse versie van de tekst",
     },
      Muldumarec: {
       title: 'Muldumarec',
-      image: 'https://photos.yodayo.com/65d5c202-7b13-4a85-9506-147fda2e92d6_width=1200&height=1200.webp',
-      info: 'Muldumarec verbindt geheime liefde met de latere publieke waarheid.',
-      quote: 'Nauwelijks had ze zo gesproken, of ze zag de schaduw van een grote vogel aan een smal venster. De vogel vloog de kamer in. Hij leek op een havik en had riempjes aan zijn poten. Hij ging voor haar zitten en veranderde na een tijdje in een mooie, gracieuze ridder.',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Codex_Manesse_Hartmann_von_Aue.jpg/250px-Codex_Manesse_Hartmann_von_Aue.jpg',
+      info: 'In de lai van Yonec is de ridder Muldumarec een wonderlijk wezen dat zich in een vogel kan veranderen. Hij is geen gewone ridder: hij komt uit een andere wereld en bezoekt de dame die opgesloten zit in haar donjon. Hij wordt beschreven als "knap en gracieus" en vertegenwoordigt de ideale, onmogelijke liefde.',
+      quote: '"Nauwelijks had ze zo gesproken, of ze zag de schaduw van een grote vogel aan een smal venster. De vogel vloog de kamer in. Hij leek op een havik en had riempjes aan zijn poten. Hij ging voor haar zitten en veranderde na een tijdje in een mooie, gracieuze ridder."',
       source: "Voor de bron, klik op de Franse versie van de tekst",
     },
      jaloux: {
       title: 'De jaloerse oude man',
       image: 'https://cdn.slate.fr/uploads/store/story_157111/large_landscape_157111.jpg',
-      info: 'De jaloerse echtgenoot is de centrale figuur van de onderdrukking in Yonec. Hij sluit zijn jonge en mooie vrouw meer dan zeven jaar op in een donjon uit angst bedrogen te worden. De dame zelf roept uit: "Wat vreest die jaloerse oude man toch dat hij mij zo wreed opsluit?" Zijn jaloezie drijft hem ertoe een dodelijke val voor Muldumarec te spannen met ijzeren pinnen. Hij vertegenwoordigt het middeleeuwse gearrangeerde huwelijk en de onderdrukking van de vrouw.',
+      info: 'De jaloerse echtgenoot is de centrale figuur van de onderdrukking in Yonec. Hij sluit zijn jonge en mooie vrouw op in een donjon uit angst bedrogen te worden. De dame zelf roept uit: "Wat vreest die jaloerse oude man toch dat hij mij zo wreed opsluit?" Zijn jaloezie drijft hem ertoe een dodelijke val voor Muldumarec te spannen met ijzeren pinnen. Hij vertegenwoordigt het middeleeuwse gearrangeerde huwelijk en de onderdrukking van de vrouw.',
+      quote: '" Omdat ze zo mooi en gracieus was, dacht hij er alleen aan haar te bewaken."',
+      source: "Voor de bron, klik op de Franse versie van de tekst",
+    },
+     seigneur: {
+      title: 'De heer / De jaloerse oude man',
+      image: 'https://cdn.slate.fr/uploads/store/story_157111/large_landscape_157111.jpg',
+      info: 'De jaloerse echtgenoot is de centrale figuur van de onderdrukking in Yonec. Hij sluit zijn jonge en mooie vrouw op in een donjon uit angst bedrogen te worden. De dame zelf roept uit: "Wat vreest die jaloerse oude man toch dat hij mij zo wreed opsluit?" Zijn jaloezie drijft hem ertoe een dodelijke val voor Muldumarec te spannen met ijzeren pinnen. Hij vertegenwoordigt het middeleeuwse gearrangeerde huwelijk en de onderdrukking van de vrouw.',
       quote: '" Omdat ze zo mooi en gracieus was, dacht hij er alleen aan haar te bewaken."',
       source: "Voor de bron, klik op de Franse versie van de tekst",
     },
      amour: {
-      title: 'Hoofse liefde',
+      title: 'Hoofse liefde / Fin\'amor',
       image: 'https://medias.histoire-et-civilisations.com/api/v1/images/view/62f21a9eec780671cd3085c5/width_1000/image.jpg',
-      info: 'De hoofse liefde is een ideologie die de liefdesrelaties in de middeleeuwen codeert. In Yonec formuleert de dame zelf haar verlangen naar een minnaar "mooi en hoofs, dapper en deudig". Het is haar woord dat Muldumarec doet verschijnen. Het geheim is fundamenteel: als de liefde wordt onthuld, leidt dit tot de dood. De hoofse liefde in Yonec is dus zowel een bron van geluk als van tragedie. De helden van de Lais, net als die van volkssprookjes, zijn op zoek naar geluk: ze zullen het vinden in de liefde, zoals de ongelukkig gehuwde vrouw uit Yonec. Maar deze liefde is onlosmakelijk verbonden met lijden, een lijden dat de geliefden uiteindelijk in de dood zal herenigen.',
-      quote: '"Het jonge meisje dat men hem gaf, was van hoge afkomst, wijs en hoffelijk, en uitzonderlijk mooi. Hij werd meteen op haar schoonheid verliefd."',
+      info: 'De uitdrukking hoofse liefde of fin’amor verwijst naar de manier waarop iemand met hoffelijkheid, respect en eerlijkheid van zijn of haar partner kan houden, met als doel om de vreugde en geluk te bereiken. Een voorwaarde is echter wel vaak dat beide partijen de regels van het (liefdes)spel volgen. De Lais de Marie staan bekend om hun viering van de liefde. De ongelukkige getrouwde vrouw (malmariée), probeert liefde en geluk te vinden bij de vogel-ridder Muldumarec en overtreedt hiermee de sociale regels (ze gaat vreemd, ze liegt en bedriegt), waardoor het verhaal uiteindelijk slecht afloopt. Het enige positieve is dat de twee geliefden worden herenigd na de dood. De dame wordt begraven in hetzelfde graf als haar minnaar, en Yonec wordt heer van het land.  De lai waarschuwt de lezer voor zelfzuchtige liefde. ',
+      quote: '“Ze had vaak gehoord dat er vroeger in dit land wonderlijke avonturen gebeurden die ongelukkigen weer vreugde brachten: ridders vonden hun droomvrouw, en dames vonden minnaars, mooi, hoffelijk en dapper, zonder verwijt, omdat alleen zij hen konden zien.”',
       source: "Voor de bron, klik op de Franse versie van de tekst",
     },
      dieu: {
@@ -199,7 +209,7 @@ Zo werd dit verhaal later een lai, om de pijn en het verdriet van deze twee geli
     lai: {
       title: 'Lai',
       image: '/images/afblai.png',
-      info: 'Oorspronkelijk was de lai een kort lyrisch gedicht dat werd gezongen door Bretonse jongleurs die zichzelf begeleidden op de Keltische harp. De lai vertelt een liefdesverhaal en put daarbij uit een rijke bron van legenden en het wonderbaarlijke. Het lied verandert zo in een klein verhaal, waarin de verbeelding een belangrijke plaats inneemt. In de lai van Yonec kan een ridder zich in een havik veranderen.',
+      info: 'Oorspronkelijk was de lai een kort lyrisch gedicht dat werd gezongen door Bretonse jongleurs die zichzelf begeleidden op de Keltische harp. De lai vertelt een liefdesverhaal en put daarbij uit een rijke bron van legenden en het wonderbaarlijke. Het lied verandert zo in een klein verhaal, waarin de verbeelding een belangrijke plaats inneemt. Deze overblijfselen van een orale traditie hebben het kenmerk dat ze de grens tussen geschiedenis en legende, het wonderlijke en de realiteit opheffen. In de lai van Yonec kan een ridder zich in een valk veranderen. Wanneer de Dame hem volgt, komt ze in een andere (magische) wereld terecht.',
       quote: 'Lai: een kort verhaal in versvorm, gebaseerd op verloren gegane Bretonse liederen, waarbij elke versregel acht lettergrepen heeft en twee opeenvolgende versregels op elkaar rijmen.',
       source: "Voor de bron, klik op de Franse versie van de tekst",
     },
@@ -208,6 +218,20 @@ Zo werd dit verhaal later een lai, om de pijn en het verdriet van deze twee geli
       image: 'https://www.readingmuseum.org.uk/sites/default/files/marie_de_france.png',
       info: 'De meeste lais zijn anoniem, maar we kennen de auteur van een reeks lais die werden geschreven tussen 1160 en 1178: Marie de France. Het leven van Marie de France blijft vaag: men weet dat ze aan het hof van Hendrik II, koning van Engeland, verkeerde, en dat is alles. Ze presenteert haar werk als de vertaling in versvorm van verhalen die ze in Bretagne had gehoord. Ze wil "in rijm vertellen" over avonturen en legenden. We vinden dan ook de gebruikelijke elementen van het wonderbaarlijke terug: een overgang naar een hiernamaals of een denkbeeldig land, gedaantewisselingen van dieren en mensen, voorspellingen en wonderen, enzovoort. Alle gedichten van Marie draaien rond het avontuur van het liefhebben, met de nadruk op het pathetische en het gevoelsmatige, zelfs het melancholische.',
       quote: '"Ikzelf ken er vele en ik wil niet dat ze verloren gaan. Daarom heb ik ze omgezet in rijmende verhalen, die mij menige slapelloze nacht hebben gekost." - Marie de France',
+      source: "Voor de bron, klik op de Franse versie van de tekst",
+    },
+    merveilleuses: {
+      title: 'Het wonderlijke',
+      image: 'https://medias-educarte.lab.arte.tv/prod/medias/illustrations/thematic_1863_affiche.JPG',
+      info: 'De lai Yonec heeft als kenmerk dat het de grens tussen geschiedenis en legende, het wonderlijke en de realiteit opheft. Verschillende wonderlijke aspecten komen aan bod. Zo kan de ridder Muldumarec zichzelf in een vogel veranderen. Wanneer de Dame hem volgt, komt ze in een andere (magische) wereld terecht. Hij geeft haar een magische ring die ervoor zorgt dat haar jaloerse oude echtgenoot haar ontrouw vergeet. Hoewel dit verhaal veel wonderbaarlijke elementen bevat, hebben ze allemaal de echte wereld als achtergrond, met een hoge pessimistische conclusie waarin pijn en beproeving de vreugde en het aanvankelijke geluk opvolgen. ',
+      quote: '“Ze ging naar binnen, door de duisternis heen, en kwam uit in een prachtige weide. Van daar zag ze een schitterende stad met muren, torens en zalen die van zilver leken.”',
+      source: "Voor de bron, klik op de Franse versie van de tekst",
+    },
+    chevalier: {
+      title: 'De magische ridder',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Codex_Manesse_Hartmann_von_Aue.jpg/250px-Codex_Manesse_Hartmann_von_Aue.jpg',
+      info: 'Het verhaal van Yonec is onder andere gebaseerd op het thema van de magische ridder, die kan veranderen in een vogel. Hij beantwoord de oproep van de prachtige dame die klaagt over haar lot. De dame wenst een geliefde, waarop Muldumarec verschijnt als een knappe ridder die hoffelijk tegen haar spreekt en haar verzekert dat hij uit liefde naar haar toe kwam. De ridder is een hoffelijk en magisch wezen dat uit een andere wereld lijkt te komen. Hij staat symbool voor perfecte, maar onbereikbare liefde. Ook Yonec, de zoon van Muldumarec en de dame, bereikt de leeftijd waarop hij een charmante ridder is. Yonec dood de jaloerse oude man met een zwaard om hem te wreken en wordt vervolgens heer van het (magische) land.',
+      quote: '“Hij landt voor de dame: na enige tijd, wanneer zij hem lang heeft aanschouwd, verandert hij in een knappe en gracieuze ridder. De dame is getuige van dit wonder: haar hart slaat een slag van schrik, en uit angst bedekt zij haar hoofd met haar sluier. Maar de ridder spreekt haar hoffelijk toe: "Wees niet bang, dame, de havik is een edele vogel!"',
       source: "Voor de bron, klik op de Franse versie van de tekst",
     },
   },
